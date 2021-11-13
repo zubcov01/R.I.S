abb96d50185ae493846d857bbfc85b5a948a4d92
zubcov01 <zubcov-07@mail.ru>
2021-11-13 14:23:23 +0600

    second commit

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.burger').classList.toggle('active');
+    document.querySelector('nav').classList.toggle('active');
+});
